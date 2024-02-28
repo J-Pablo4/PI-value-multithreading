176ccaccd43b8a5ea6cdfec2ef1655a42fa06cfe
J-Pablo4 <73197689+J-Pablo4@users.noreply.github.com>
2024-02-28 11:21:13 -0600

    Added comments

--- pi.c
+++ pi.c
@@ -7,56 +7,59 @@ Equipo: Quokka
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <sys/wait.h>
 #include <unistd.h>
 #include <math.h>
 
-#define LOWER_LIMIT 0
-#define UPPER_LIMIT 1
-#define NUMBER_OF_TRAPEZES 99999999
+#define LOWER_LIMIT 0 //Empieza el 1/4 de circulito
+#define UPPER_LIMIT 1 //Termina el 1/4 de circulito
+#define NUMBER_OF_TRAPEZES 999999999 //Cuantos trapecios tiene el circulito (osease n)
 
 int main(int argc, char **argv)
 {
+	//Variables del Tiempo
 	long long start_ts;
 	long long stop_ts;
 	long long elapsed_time;
 	struct timeval ts;
 	
+	//Variables del circulo
 	float x = 0;
     	double sum_x = 0;
     	double func_eval = 0;
     	float delta_x;
-    	double total = 0;
 
-    	gettimeofday(&ts, NULL);
+    	gettimeofday(&ts, NULL);//Toma el tiempo inicial de la computadora
     	start_ts = ts.tv_sec; // Tiempo inicial
     
-	delta_x = (UPPER_LIMIT - LOWER_LIMIT) / (float)NUMBER_OF_TRAPEZES;
+	delta_x = (UPPER_LIMIT - LOWER_LIMIT) / (float)NUMBER_OF_TRAPEZES; //Base de los trapecios (delta x = (b-a)/n)
 
+	//2*f(xn-1) Calculo de los trapecios de en medio
 	for(long double i = 1; i < NUMBER_OF_TRAPEZES; i++)
 	{
-		x = (float)LOWER_LIMIT + (i * delta_x);
-		func_eval = (sqrt(1 - (x*x))*2);
+		x = (float)LOWER_LIMIT + (i * delta_x); //xi = a+i*delta x
+		func_eval = (sqrt(1 - (x*x))*2); //2*f(xn-1)
 		sum_x += func_eval;
 		printf("i: %Lf\n", i);
 	}
 
-	x = (float)LOWER_LIMIT + (0.0 * delta_x);
-	func_eval = sqrt(1 - (x*x));
+	//f(x0) Calculo del primer trapecio
+	x = (float)LOWER_LIMIT + (0.0 * delta_x); //x0 = a+0*delta x
+	func_eval = sqrt(1 - (x*x)); //f(x0)
 	sum_x += func_eval;
 
-	x = (float)LOWER_LIMIT + ((double)NUMBER_OF_TRAPEZES * delta_x);
-    	func_eval = sqrt(1 - (x*x));
+	//f(xn) Calculo del ultimo trapecio
+	x = (float)LOWER_LIMIT + ((double)NUMBER_OF_TRAPEZES * delta_x); //xn = a+n*delta x
+    	func_eval = sqrt(1 - (x*x)); //f(xn)
     	sum_x += func_eval;
 
-    	sum_x *= delta_x;
-    	sum_x /= 2.0;
+    	sum_x *= delta_x; //delta x * sumatoria de todas las areas
+    	sum_x /= 2.0; //sumatoria de todas las areas entre 2
 
     	printf("Valor de PI: %.15lf\n", sum_x * 4.0);
 
-	gettimeofday(&ts, NULL);
+	gettimeofday(&ts, NULL); //Toma el tiempo final de la computadpra
     	stop_ts = ts.tv_sec; // Tiempo final
-    	elapsed_time = stop_ts - start_ts;
+    	elapsed_time = stop_ts - start_ts; //Calculo del tiempo de ejecucion
 
     	printf("------------------------------\n");
     	printf("TIEMPO TOTAL, %lld segundos\n",elapsed_time);
